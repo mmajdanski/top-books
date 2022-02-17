2498d2f3eb1c64dba0211e5974ab2cb24603efd1
Michael Majdanski <35552662+mmajdanski@users.noreply.github.com>
2022-02-17 01:02:07 -0700

    style changes and added filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ const axios = require("axios");
 const jsdom = require("jsdom");
 import Head from "next/head";
 import { GetStaticProps } from "next";
+import { useEffect, useState } from "react";
 
 import styles from "../styles/Home.module.css";
 import BookCard from "../components/BookCard";
@@ -24,24 +25,77 @@ type Props = {
   books: Book[];
 };
 
+const returnBookCards = (booksFilteredState: Book[]) => {
+ if(booksFilteredState.length == 0){
+   let no_books_found: Book = {
+     title: "No books found",
+     author: "No authors found",
+     type: "No type found",
+     discount_price: "$00.00",
+     real_price: "$00.00",
+     num_reviews: "0",
+     image_url: "https://cdn.pixabay.com/photo/2021/07/07/22/48/question-mark-6395460_960_720.jpg",
+     index: 0
+   }
+   return <BookCard {...no_books_found} />
+ }
+  return booksFilteredState.map((booksFilteredState) => {
+    return <BookCard key={booksFilteredState.title} {...booksFilteredState} />;
+  })
+
+}
+
 const Home: NextPage<Props> = ({ books }: Props) => {
+
+  const [booksFilteredState, setBooksFilteredState ] = useState(books);
+
+  // useEffect(() => {
+  //   // returnBookCards(booksFilteredState);
+  // }, [booksFilteredState])
+
   return (
-    <div className={styles.container}>
+    <div className="">
       <Head>
         <title>Top 20 Book List</title>
         <meta name="description" content="Amazon Top 20 Book List" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className="bg-cyan-700">
+        <h1 className="text-5xl text-center font-normal mt-10 pt-10 text-white">
+          Best Sellers
+        </h1>
+        <h3 className="text-lg text-center pt-5 pb-10 text-white">
+          The most popular books based on sales. Updated daily.
+        </h3>
+      </div>
+      <div className="main-body-container flex flex-row">
+        <div className="sidebar w-64 min-h-full border-r-2  border-slate-500 ">
+          <h2 className="my-4 ml-2 text-2xl font-semibold">Filters</h2>
+          <hr />
+          <ul className="m-4">
+            <h2 className=" text-xl font-medium">Rating:</h2>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.index <5)])}>Top 4</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.index <9)])}>Top 8</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.index <13)])}>Top 12</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.index <17)])}>Top 16</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.index <20)])}>Top 20</li>
+          </ul>
+          <hr />
+          <ul className="m-4">
+            <h2 className=" text-xl font-medium">Cost:</h2>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.discount_price < "$1")])}>$0 - $10</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.discount_price < "$2" && book.discount_price > "$1")])}>$10 - $20</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.discount_price < "$3" && book.discount_price > "$2")])}>$20 - $30</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState([...books.filter(book => book.discount_price > "$3")])}>$30 +</li>
+            <li className="transition ease-in-out hover:cursor-pointer hover:bg-slate-100 duration-300 hover:-translate-y-1 hover:scale-105" onClick={() => setBooksFilteredState(books)}>All</li>
+          </ul>
+          <hr />
+        </div>
+        <div className="grid grid-cols-4 gap-3 justify-items-center mx-40  shadow-xl shadow-slate-200 py-10 my-10">
 
-      <h1 className="text-3xl text-center font-normal my-10">
-        Amazon Top 20 Book List
-      </h1>
-      <div className="grid grid-cols-4 gap-3 justify-items-center mx-40  shadow-xl shadow-slate-200 py-10 my-10">
-      {books.map((book) => {
-        return (
-          <BookCard key={book.title} {...book} />
-        );
-      })}
+          {returnBookCards(booksFilteredState)}
+
+        </div>
       </div>
     </div>
   );
